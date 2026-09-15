2e3d1a05e401c95f6bc57f636455c2419a8c69d2
Brian Monroe <bmonroe@ucsb.edu>
2026-09-15 09:12:20 -0700

    Update command for findingzui
    Signed-off-by: Brian Monroe <bmonroe@ucsb.edu>

--- extra_config.py
+++ extra_config.py
@@ -1,12 +1,11 @@
 c.ServerProxy.servers = {
     "streamlit_ui": {
-        "command" : ["python3", "-c", "from pyspark.context import SparkContext; SparkContext()"],
+        "command" : ["conda", "run", "-n", "hep", "findingz-ui --server.port=8501"],
         "port" : 8501,
         "absolute_url": False,
         "timeout" : 3600,
         "launcher_entry": {
                 "enabled": True,
-
                 "icon_path": "/opt/streamlit-mark-color.svg",
                 "title": "Streamlit UI",
                 "path_info": "streamlit/"
